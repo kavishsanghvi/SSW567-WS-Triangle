8c29e40c4df9638ed80770cf682bb94b87fc7c38
Kavish Sanghvi <sanghvi_kavish@yahoo.in>
2021-09-17 12:32:55 -0400

    add test cases

---
+++ TestTriangle.py
@@ -0,0 +1,29 @@
+"""
+@author: kavishsanghvi
+@purpose: to demonstrate a simple unittest implementation
+"""
+
+import unittest
+from Triangle import classifyTriangle
+
+class TestTriangles(unittest.TestCase):
+
+    def testRightTriangle(self): 
+        self.assertEqual(classifyTriangle(3, 4, 5),'Right')
+        self.assertEqual(classifyTriangle(5, 3, 4),'Right')
+        
+    def testEquilateralTriangle(self): 
+        self.assertEqual(classifyTriangle(1, 1, 1),'Equilateral')
+        self.assertEqual(classifyTriangle(5, 5, 5), 'Equilateral')
+
+    def testIsoscelesTriangle(self):
+        self.assertEqual(classifyTriangle(150, 155, 155), 'Isosceles')
+        self.assertEqual(classifyTriangle(2, 3, 3), 'Isosceles')
+        
+    def testScaleneTriangle(self):
+        self.assertEqual(classifyTriangle(1, 2, 3), 'Scalene')
+        self.assertEqual(classifyTriangle(6, 5, 4), 'Scalene')
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
